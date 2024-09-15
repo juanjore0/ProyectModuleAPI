82bd544264da83414ddf632975f2f6d1738f365d
Juan Jose Arango Orozco <136941334+juanjore0@users.noreply.github.com>
2024-09-14 21:32:50 -0500

    .

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,7 @@
 """"This module contains the functions to connect to the API and get the data from it"""
 import pandas as pd
 from sodapy import Socrata
+import numpy as np
 
 def create_client():
     """Create a client to connect to the API"""
@@ -13,10 +14,22 @@ def get_data(number, department, client):
 def convert_dataframe(data):
     """Convert the data to a DataFrame"""
     return pd.DataFrame.from_records(data)
+#NUEVO
+def clean_data(data):
+    """Clean the data"""
+    data.replace("N/A", np.nan, inplace=True)
+    data = data.dropna(subset=["ciudad_municipio_nom",
+        "departamento_nom", 
+        "edad", 
+        "fuente_tipo_contagio",
+        "estado", 
+        "recuperado"])
+    return data
 
 def format_df(data):
     """Format the DataFrame"""
     pd.set_option('display.max_rows', None)
     pd.set_option('display.max_columns', None)
-    list_data = ["ciudad_municipio_nom", "departamento_nom", "edad", "sexo", "estado", "fuente_tipo_contagio"]
+    list_data = ["ciudad_municipio_nom", "departamento_nom", "edad",
+    "sexo", "estado", "fuente_tipo_contagio"]
     return data[list_data]
--- ui/__init__.py
+++ ui/__init__.py
@@ -1,4 +1,5 @@
 """This module is in charge of the user interface of the program"""
+import matplotlib.pyplot as plt
 
 def request_number():
     """Request the data to the user"""
@@ -10,9 +11,19 @@ def request_deparment():
     print("INGRESE EL NOMBRE DEL DEPARTAMENTO ")
     return input().upper()
 
+def graph_data(data):
+    """Graph the data"""
+    data["edad"] = data["edad"].astype(int)
+    data["edad"].plot(kind="hist", bins=10, edgecolor="black")
+    plt.xlabel("Edad")
+    plt.ylabel("Número de casos")
+    plt.title("Número de casos por edad")
+    plt.grid(True, which="both", axis="both", linestyle="--", linewidth=0.5)  # Agregar cuadrícula
+    plt.show()
+
 def show_data(data):
     """"display the data"""
-    renamed_df = data.rename(columns={ 
+    renamed_df = data.rename(columns={
         "ciudad_municipio_nom": " Ciudad ", 
         "departamento_nom": " Departamento ", 
         "edad" : "edad", 
